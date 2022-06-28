a0c07c263472677ee11097448c7e869f340de519
Oyebola Ebunoluwa <oyebolaebun07@gmail.com>
2022-06-28 15:40:22 +0100

    created functions and states in context

--- src/context.js
+++ src/context.js
@@ -1,9 +1,41 @@
 import React, { useState, useContext } from "react";
 
 const AppContext = React.createContext();
+//context is used because the buttons are in separate components and to avoid prop drilling
+//Because the buttons are on different components, you would need separate functions for show and hide
+//This would cause App.js to be bulky hence the need for context.
 
 function AppProvider({ children }) {
-  return <AppContext.Provider value={"hello"}>{children}</AppContext.Provider>;
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  function openSidebar() {
+    setIsSidebarOpen(true);
+  }
+  function closeSidebar() {
+    setIsSidebarOpen(false);
+  }
+  function openModal() {
+    setModalOpen(true);
+  }
+  function closeModal() {
+    setModalOpen(false);
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        isModalOpen,
+        isSidebarOpen,
+        openModal,
+        openSidebar,
+        closeModal,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 }
 
 //custom hook
